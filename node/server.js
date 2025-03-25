fd8fa14f829b46156422867f8b46bd7c0c137966
FireDemon2903 <firedemon2903@gmail.com>
2025-03-25 13:49:38 +0100

    progress
    WHY IS PATH THERE TWICE FUCK ME

--- node/server.js
+++ node/server.js
@@ -4,7 +4,7 @@ export { startServer, respondError, htmlResponse, jsonResponse, fileResponse };
 
 import fs from 'fs';
 //import url from 'url';
-//import path from 'path';
+import path from 'path';
 import process from 'process';
 import * as utils from './utils.js';
 import http from 'http';
@@ -15,8 +15,8 @@ import { log } from "console";
 const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 
-//const rootDir = process.cwd();
-//const publicDir = path.join(rootDir, 'public'); // as of yet unused
+const rootDir = process.cwd();
+const publicDir = path.join(rootDir, 'public'); // as of yet unused
 
 
 // ----------------- Response Handlers -----------------
@@ -30,14 +30,14 @@ function respondError(res, error) {
 }
 
 /* send a response with htmlString as html page */
-function htmlResponse(res, htmlString){
+function htmlResponse(res, htmlString) {
     res.statusCode = 200;
     res.setHeader('Content-Type', "text/html");
     res.write(htmlString);
     res.end('\n');
 }
 
-function jsonResponse(res, json){
+function jsonResponse(res, json) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.write(JSON.stringify(json));
@@ -45,7 +45,8 @@ function jsonResponse(res, json){
 }
 
 function fileResponse(res, path) {
-    // todo: secure path
+    path = securePath(path);
+    if (!path) throw new NoResourceError('Invalid path');
     log(`Serving file: ${path}`);
     fs.readFile(path, (err, data) => {
         if (err) {
@@ -58,33 +59,46 @@ function fileResponse(res, path) {
             res.end('\n');
         }
     });
-}   
+}
 
 // ripped from lecture notes, use mmagic instead later, if needed/want to make better
 //A helper function that converts filename suffix to the corresponding HTTP content type
 //better alternative: use require('mmmagic') library
-function guessMimeType(fileName){
-    const fileExtension=fileName.split('.').pop().toLowerCase();
+function guessMimeType(fileName) {
+    const fileExtension = fileName.split('.').pop().toLowerCase();
     console.log(fileExtension);
-    const ext2Mime ={ //Aught to check with IANA spec
-      "txt": "text/txt",
-      "html": "text/html",
-      "ico": "image/ico", // CHECK x-icon vs image/vnd.microsoft.icon
-      "js": "text/javascript",
-      "json": "application/json", 
-      "css": 'text/css',
-      "png": 'image/png',
-      "jpg": 'image/jpeg',
-      "wav": 'audio/wav',
-      "mp3": 'audio/mpeg',
-      "svg": 'image/svg+xml',
-      "pdf": 'application/pdf',
-      "doc": 'application/msword',
-      "docx": 'application/msword'
-     };
-      //incomplete
-    return (ext2Mime[fileExtension]||"text/plain");
-  }
+    const ext2Mime = { //Aught to check with IANA spec
+        "txt": "text/txt",
+        "html": "text/html",
+        "ico": "image/ico", // CHECK x-icon vs image/vnd.microsoft.icon
+        "js": "text/javascript",
+        "json": "application/json",
+        "css": 'text/css',
+        "png": 'image/png',
+        "jpg": 'image/jpeg',
+        "wav": 'audio/wav',
+        "mp3": 'audio/mpeg',
+        "svg": 'image/svg+xml',
+        "pdf": 'application/pdf',
+        "doc": 'application/msword',
+        "docx": 'application/msword'
+    };
+    //incomplete
+    return (ext2Mime[fileExtension] || "text/plain");
+}
+
+//secture file system access as described on 
+//https://nodejs.org/en/knowledge/file-system/security/introduction/
+function securePath(userPath) {
+    if (userPath.indexOf('\0') !== -1) {
+        // could also test for illegal chars: if (!/^[a-z0-9]+$/.test(filename)) {return undefined;}
+        return undefined;
+    }
+    userPath = path.normalize(userPath).replace(/^(\.\.(\/|\\|$))+/, '');
+    userPath = publicDir + userPath;
+
+    return path.join(rootDir, path.normalize(userPath));
+}
 
 // ----------------- Server -----------------
 const server = http.createServer(requestHandler);
