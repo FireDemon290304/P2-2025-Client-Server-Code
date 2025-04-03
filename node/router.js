c4c6cb24b732d382ea74af7d80a43d091e68ba1a
FireDemon2903 <firedemon2903@gmail.com>
2025-04-03 13:25:34 +0200

    reformatting

--- node/router.js
+++ node/router.js
@@ -1,15 +1,27 @@
 import express from 'express';
 import path from 'path';
-import { log, NoResourceError } from './utils.js'
+import { log, InternalError, NoResourceError } from './utils.js'
 
 const router = express.Router();
 const publicDir = path.join(process.cwd(), 'public');
 
 // ----------------- routes -----------------
+// logging middleware
+router.use((req, res, next) => {
+    log(`GOT: ${req.method} ${req.url}`);
+    next();
+});
+
+// errors middleware
+router.use((err, req, res, next) => {
+    log(new InternalError(err.message), true);
+    res.status(500).send('500: Internal Server Error');
+    next();
+});
+
 router.get('/', (req, res) => {
     res.sendFile(path.join(publicDir, 'html', 'index.html'));
 });
-
 
 // Unknown routes
 router.use((req, res) => {
--- node/server.js
+++ node/server.js
@@ -7,7 +7,7 @@ import process from 'process';
 import dotenv from 'dotenv';
 
 import router from './router.js'
-import { log, InternalError } from "./utils.js";
+import { log } from "./utils.js";
 export { startServer };
 
 // ----------------- Basic configs -----------------
@@ -20,22 +20,6 @@ const PORT = process.env.PORT || 3240;  // node0: 3240, node9: 3249
 const rootDir = process.cwd();
 const publicDir = path.join(rootDir, 'public');
 app.use(express.static(publicDir));
-
-// ----------------- Logging -----------------
-// logging middleware
-app.use((req, res, next) => {
-    log(`GOT: ${req.method} ${req.url}`);
-    next();
-});
-
-// errors middleware
-app.use((err, req, res, next) => {
-    log(new InternalError(err.message), true);
-    res.status(500).send('500: Internal Server Error');
-    next();
-});
-
-// place this after middleware
 app.use(router);
 
 // ----------------- Server -----------------
